97af9feff1bbadb4c81c2fd4194d30a9ea0c4821
peichen <1981336562@qq.com>
2025-12-05 14:36:27 +0800

    fix: 启用CORS以解决跨域问题

--- be/src/main.ts
+++ be/src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from "@nestjs/config";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
